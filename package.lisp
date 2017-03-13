f221cac93a9defff9ee9efc2d6c044b6b740a885
Shinichi Sato <hyotang.karakoma@gmail.com>
2017-03-14 01:45:37 +0900

    fix:

--- package.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :cl-user)
-
-(defpackage :with-package
-  (:use :cl)
-  (:export #:with-import
-	   #:with-use-package
-	   #:dangerous-use-package
-	   #:most-dangerous-use-package
-	   #:find-conflict))
