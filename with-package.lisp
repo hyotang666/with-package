7d5a917dec9ba5d40148bd8a1705d06ec6ce747a
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-06-27 10:17:19 +0900

    Cleanup: Format.

--- with-package.lisp
+++ with-package.lisp
@@ -1,83 +1,83 @@
 (in-package :cl-user)
-(defpackage :with-package (:use :cl :named-readtables)
+
+(defpackage :with-package
+  (:use :cl :named-readtables)
   (:import-from :trestrul #:mapleaf)
-  (:export
-    ;;;; main api
-    #:with-import
-    #:with-use-package
-    ;;;; readtable-control
-    #:|#@-reader|
-    #:enable
-    #:syntax ; readtable name.
-    ;;;; condition
-    #:package-missing
-    ))
+  (:export ;;;; main api
+           #:with-import
+           #:with-use-package
+           ;;;; readtable-control
+           #:|#@-reader|
+           #:enable
+           #:syntax ; readtable name.
+           ;;;; condition
+           #:package-missing))
+
 (in-package :with-package)
 
-(define-condition package-missing(error)
+(define-condition package-missing (error)
   ((api :initarg :api :reader api)
    (name :initarg :name :reader name))
-  (:report(lambda(condition *standard-output*)
-	    (format t "~A: Package ~S is not found.~%"
-		    (api condition)(name condition)))))
+  (:report
+   (lambda (condition *standard-output*)
+     (format t "~A: Package ~S is not found.~%" (api condition)
+             (name condition)))))
 
-(defun package-missing(api package-name)
-  (error(make-condition 'package-missing
-			:api api
-			:name package-name)))
+(defun package-missing (api package-name)
+  (error (make-condition 'package-missing :api api :name package-name)))
 
-(defmacro with-import((package &rest symbols)&body body)
-  (let((*package*(or (find-package package)
-		     (package-missing 'with-import package))))
-    `(PROGN ,@(treatment symbols body))))
+(defmacro with-import ((package &rest symbols) &body body)
+  (let ((*package*
+         (or (find-package package) (package-missing 'with-import package))))
+    `(progn ,@(treatment symbols body))))
 
-(defun treatment(symbols body)
-  (labels((TREAT(leaf)
-	    (cond
-	      #+sbcl
-	      ((sb-int:comma-p leaf)
-	       (let((expr(sb-int:comma-expr leaf)))
-		 (sb-int:unquote (if(consp expr)
-				   (treatment symbols expr)
-				   (TREAT expr))
-				 (sb-int:comma-kind leaf))))
-	      ((not(target-symbolp leaf))
-	       leaf)
-	      ((member leaf symbols :test #'string=)
-	       (intern(string leaf)))
-	      (t leaf))))
-    (mapleaf #'TREAT body)))
+(defun treatment (symbols body)
+  (labels ((treat (leaf)
+             (cond #+sbcl
+                   ((sb-int:comma-p leaf)
+                    (let ((expr (sb-int:comma-expr leaf)))
+                      (sb-int:unquote
+                        (if (consp expr)
+                            (treatment symbols expr)
+                            (treat expr))
+                        (sb-int:comma-kind leaf))))
+                   ((not (target-symbolp leaf)) leaf)
+                   ((member leaf symbols :test #'string=)
+                    (intern (string leaf)))
+                   (t leaf))))
+    (mapleaf #'treat body)))
 
-(defun target-symbolp(arg)
+(defun target-symbolp (arg)
   (and (typep arg '(and symbol (not (or keyword boolean))))
        (symbol-package arg)))
 
-(defmacro with-use-package((package &key except with-internal)&body body)
-  (let((*package*(or (find-package package)
-		     (package-missing 'with-use-package package))))
-    `(PROGN
-       ,@(treatment 
-	   (symbols-but-except package except with-internal)
-	   body))))
+(defmacro with-use-package ((package &key except with-internal) &body body)
+  (let ((*package*
+         (or (find-package package)
+             (package-missing 'with-use-package package))))
+    `(progn
+      ,@(treatment (symbols-but-except package except with-internal) body))))
 
-(defun symbols-but-except(package except with-internal)
+(defun symbols-but-except (package except with-internal)
   (loop :for symbol :being :each :external-symbol :in package
-	:collect symbol :into black-list
-	:finally (return(nconc (uiop:ensure-list with-internal)
-			       (set-exclusive-or black-list
-						 (uiop:ensure-list except)
-						 :test #'string=)))))
+        :collect symbol :into black-list
+        :finally (return
+                  (nconc (uiop:ensure-list with-internal)
+                         (set-exclusive-or black-list (uiop:ensure-list except)
+                                           :test #'string=)))))
 
-(defun |#@-reader|(stream character number)
-  (declare(ignore character number))
-  (let((package(read stream t t t)))
-    (if(atom package)
-      `(WITH-USE-PACKAGE(,package),(read stream t t t))
-      `(WITH-IMPORT ,package ,(read stream t t t)))))
+(defun |#@-reader| (stream character number)
+  (declare (ignore character number))
+  (let ((package (read stream t t t)))
+    (if (atom package)
+        `(with-use-package (,package)
+           ,(read stream t t t))
+        `(with-import ,package
+           ,(read stream t t t)))))
 
-(defmacro enable()
-  `(EVAL-WHEN(:COMPILE-TOPLEVEL :LOAD-TOPLEVEL :EXECUTE)
-     (SET-DISPATCH-MACRO-CHARACTER #\# #\@ #'|#@-reader|)))
+(defmacro enable ()
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (set-dispatch-macro-character #\# #\@ #'|#@-reader|)))
 
 (defreadtable syntax
   (:merge :standard)
